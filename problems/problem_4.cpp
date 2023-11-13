ff9653eeecc53f4df5ed0112ded5fef27f91d193
SHohruhMamatov <145185852+SHohruhMamatov@users.noreply.github.com>
2023-11-13 08:44:01 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,15 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    std::string result;
+    int octet = macAddress[0]+macAddress[1];
+    if(octet/2==0){
+        result ='Unicast';
+    }else if(octet/2 !=0){
+        result = 'Multicast';
+    }else{
+        result = 'Broadcast';
+    }
     // make use of control flow statements
-    // return result;
+    return result;
 }
