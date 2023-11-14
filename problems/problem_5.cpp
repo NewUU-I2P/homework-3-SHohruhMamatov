860734069aa45013e42487062c0bf2f9e1b77083
SHohruhMamatov <145185852+SHohruhMamatov@users.noreply.github.com>
2023-11-14 20:59:51 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,21 +1,7 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-   switch(operation){
-      case "+" :
-         result = x+y;
-         break;
-      case '-' :
-         result= x-y;
-         break;
-      case '*':
-         result = x*y;
-         break;
-      case '/' :
-         result = x/y;
-         break; 
-      default :
-         std :: cout<<"Enter true operation";
+  
          
    return result;
 }
